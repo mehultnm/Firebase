a33bcd63a037cb39e3f229c810bcd2d118458e40
mehultnm <Heet@phoenixinnovate.co.uk>
2023-07-12 18:48:05 +0530

    test

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text,} from 'react-native'
+
+const App =() => {
+  return(
+      <View style={{display:'flex',backgroundColor:'#f4f4f4',alignItems:"center",justifyContent:"center",height:'100%'}}>
+        <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
+      </View>
+  )
+}
+export default App
