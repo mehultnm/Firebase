d8d6253d90af071cb25ce621781ca5c2ad3b08e4
mehultnm <mehultandeltnm@gmail.com>
2023-07-12 18:59:53 +0530

    conflict solved

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ const App =() => {
   return(
       <View style={{display:'flex',backgroundColor:'#f4f4f4',alignItems:"center",justifyContent:"center",height:'100%'}}>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
+          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
+          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FIREBASE</Text>
       </View>
   )
 }
