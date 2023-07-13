1c7910484cf4d208108b408a92fbf377d29c1413
mehultnm <mehultandeltnm@gmail.com>
2023-07-13 18:36:31 +0530

    added changes

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ const App =() => {
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FIREBASE</Text>
+          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>ACTIONS</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>ACTIONS</Text>
