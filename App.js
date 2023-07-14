8b94f478f4dbc22e2b5978ebbf3b2983d6f4a567
mehultnm <mehultandeltnm@gmail.com>
2023-07-14 10:40:24 +0530

    test14-6

--- App.js
+++ App.js
@@ -3,13 +3,10 @@ import {View, Text, Image, ImageBackground} from 'react-native';
 
 const App =() => {
   return(
-      <View style={{backgroundColor:'#f4f4f4',alignItems:"center",justifyContent:"center",height:'100%',}}>
+      <View style={{backgroundColor:'#f8f8f8',alignItems:"center",justifyContent:"center",height:'100%',}}>
 
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
-          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FIREBASE</Text>
-          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>ACTIONS</Text>
-          <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>ACTIONS</Text>
       </View>
