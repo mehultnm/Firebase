90f762e82fb40031a718fd0defef91db1670b12c
mehultnm <mehultandeltnm@gmail.com>
2023-07-14 10:16:11 +0530

    test14-1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {View, Text, Image, ImageBackground} from 'react-native';
 const App =() => {
   return(
       <View style={{backgroundColor:'#f4f4f4',alignItems:"center",justifyContent:"center",height:'100%',}}>
-          <ImageBackground style={{height:"100%",width:"100%",}} source={require('../FirebaseApp/src/assets/App_icon.png')}>
+
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FIREBASE</Text>
@@ -12,7 +12,6 @@ const App =() => {
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB</Text>
           <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>ACTIONS</Text>
-          </ImageBackground>
       </View>
   )
 }
