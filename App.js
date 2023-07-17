f4130e26c31fe056c4e8bb7f2b6d37879c4fa00b
mehultnm <mehultandeltnm@gmail.com>
2023-07-17 17:55:10 +0530

    new build

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ const App =() => {
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
+        <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
+        <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS TEST</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS TEST</Text>
 
