71f8f46d21b8b5622200cab813f378ae8e2017f3
mehultnm <mehultandeltnm@gmail.com>
2023-07-18 10:17:16 +0530

    new commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const App =() => {
       <View style={{backgroundColor:'#f8f8f8',alignItems:"center",justifyContent:"center",height:'100%',}}>
 
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
+        <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>WITH</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
         <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>GITHUB ACTIONS</Text>
